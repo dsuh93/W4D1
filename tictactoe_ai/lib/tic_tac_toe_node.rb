2c431401f16c08fded8eb590411f87fa07dcbfe6
Paul Ramirez <pramirez7230@bths.edu>
2020-11-16 16:27:43 -0500

    Make children method

--- tictactoe_ai/lib/tic_tac_toe_node.rb
+++ tictactoe_ai/lib/tic_tac_toe_node.rb
@@ -1,19 +1,20 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board
+  attr_reader :board, :prev_move_pos, :next_mover_mark
 
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
-    @mark = next_mover_mark
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
     # @node = TicTacToeNode.new(board, next_mover_mark)
   end
 
-  def next_mover_mark
-    @mark
-  end
-
   def losing_node?(evaluator)
+    if next_mover_mark == :x
+      if 
+
+    end
   end
 
   def winning_node?(evaluator)
@@ -23,13 +24,26 @@ class TicTacToeNode
   # the current move.
   def children
     children_array = []
-    board.rows.each_with_index do |row, r|
-      row.each_with_index do |col, c|
-        if board.rows[r][c] == []
-          board.dup[r][c] = next_mover_mark
+
+    (0...@board.rows.length).each do |row|
+      (0...@board.rows.length).each do |col|
+        pos = [row, col]
+        if @board.empty?(pos)
+          duped = @board.dup
+          duped[pos] = @next_mover_mark
+          children_array << TicTacToeNode.new(duped, self.switch, pos)
         end
       end
     end
 
+    children_array
+  end
+
+  def switch
+    if @next_mover_mark == :x
+      return :o
+    else
+      return :x
+    end
   end
 end
