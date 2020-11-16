1d25ea45014257c8ea8d30d40f6445964c182d56
Paul Ramirez <pramirez7230@bths.edu>
2020-11-16 15:29:09 -0500

    Switch driver/navigator

--- tictactoe_ai/lib/tic_tac_toe_node.rb
+++ tictactoe_ai/lib/tic_tac_toe_node.rb
@@ -1,7 +1,16 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @mark = next_mover_mark
+    # @node = TicTacToeNode.new(board, next_mover_mark)
+  end
+
+  def next_mover_mark
+    @mark
   end
 
   def losing_node?(evaluator)
@@ -13,5 +22,12 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    board.each_with_index |row, r|
+      board.each_with_index |col, c|
+        if board[r][c].empty?
+          board.dup[r][c] = next_mover_mark
+        end
+      end
+    end
   end
 end
